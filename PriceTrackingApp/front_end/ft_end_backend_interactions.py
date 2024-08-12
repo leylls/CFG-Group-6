5e91de23cc8d329a52cabc375a1e408795c3a215
Eva Perez Chirinos de Andres <e.perezchirinos@outlook.com>
2024-08-12 16:59:16 +0100

    Adding ft_end / back_end contract file

---
+++ PriceTrackingApp/front_end/ft_end_backend_interactions.py
@@ -0,0 +1,9 @@
+def get_product_data(url): # VIOLETA TODO
+    """
+    Retrieves the title and price data from an Amazon product given the url as parameter
+    :param url: str
+    :return: dict {title, price}
+    """
+
+    return {'title': 'Full length mirror 120cm Black', 'price': '£29'}
+
