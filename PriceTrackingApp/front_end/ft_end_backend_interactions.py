5d9241c20ee5358a2f17b04ce83633cc21f55789
Eva Perez Chirinos de Andres <e.perezchirinos@outlook.com>
2024-08-13 15:52:38 +0100

    Added new contracts for MainMenu Option 1

--- PriceTrackingApp/front_end/ft_end_backend_interactions.py
+++ PriceTrackingApp/front_end/ft_end_backend_interactions.py
@@ -1,9 +1,22 @@
 def get_product_data(url): # VIOLETA TODO
     """
-    Retrieves the title and price data from an Amazon product given the url as parameter
+    Url webscraping. Retrieves the full data from an Amazon product given the url as parameter.
     :param url: str
-    :return: dict {title, price}
+    :return: dict {title, currency, price, timestamp, url, email_notif}
     """
 
-    return {'title': 'Full length mirror 120cm Black', 'price': '£29'}
+    return {'title': 'Full length mirror 120cm Black',
+            'currency': '£',
+            'price': '199.99',
+            'timestamp': '2024-08-08 19:26',
+            'url': 'https://www.amazon.co.uk/dp/B0BL6GJVZS',
+            'email_notif': False}
+    # To set up 'email_notif' as False as default
 
+def add_new_tracking(product_data):
+    """
+    Adds product data into to DB.
+    :param product_data [dict {title, currency, price, timestamp, url}]
+    :return: None
+    """
+    return
