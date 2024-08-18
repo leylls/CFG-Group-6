d733f62dd3f904bc201ace3b9f5062f4cc187cb7
vivilivi <violeta.pereda@gmail.com>
2024-08-18 16:22:11 +0200

    include cron job interaction in main.py

--- PriceTrackingApp/main.py
+++ PriceTrackingApp/main.py
@@ -1,12 +1,27 @@
 from front_end.ft_end_dialogues_choice_logic import *
 from front_end.ft_end_dbinteractions import *
 import front_end.user_config as temp_user_config
+from back_end.backend_interactions import *
+import sys
 
-def run():
+def run(cron_job = False):
+    if cron_job:
+        url_list = get_monitored_urls()
+        web_scraping = get_ws_results(url_list)
+        product_id_and_url = get_product_id_and_urls()
+        tuple_results = prepare_results_for_db (web_scraping, product_id_and_url)
+        insert_results_db(tuple_results)
+
+        # Check if new prices are below users threshold
+        # Send email with all products with prices below threshold
+        return
+    
+    
     """
     App's central script.
     :return:
     """
+    app_welcome_ascii()
     wants_to_exit = False
     current_user = temp_user_config.current_user  # For testing logic before DB is fully set up
 
@@ -27,8 +42,9 @@ def run():
     return
 
 if __name__ == "__main__":
-    app_welcome_ascii()
-    run()
-
-
-
+    if len(sys.argv) > 1:                       #check if an argument is passed
+        argument = sys.argv[1].split('=',1)
+        if argument[0] == 'cron_job':
+            run(argument[1])
+    else:
+        run()
