76515a408680c5edb7b8a5556671b1de9b36d590
vivilivi <violeta.pereda@gmail.com>
2024-08-18 16:41:35 +0200

    amend db,tables and col names

--- PriceTrackingApp/back_end/backend_interactions.py
+++ PriceTrackingApp/back_end/backend_interactions.py
@@ -1,23 +1,18 @@
 from back_end.web_scraping import WebScraping
 import sqlite3
 
-#CHANGE DB NAME, TABLE NAME AND COLUMN NAME
-'''
-db_name = price_tracking_app
-table1 = product_details
-table2= webscraping_results
-'''
-
 # Get all urls user is monitoring from DB
 def get_monitored_urls():
-    conn = sqlite3.connect('price_traking_app.db') 
+    conn = sqlite3.connect('price_tracker.db') 
     cur = conn.cursor()
     monitored_urls= cur.execute(
     '''
-        SELECT p.url FROM product_details p
-        INNER JOIN webscraping_results ws ON p.product_id = ws.product_id
-        WHERE p.email_notification = 'True'
+        SELECT p.url FROM products p
+        INNER JOIN price_history h ON p.product_id = h.product_id
+        WHERE p.email_pref = 'True'
     ''')
+    cursor.close()
+    conn.close()
     return monitored_urls  #check if this returns a list
 
 # Web scrape url_list
@@ -27,13 +22,15 @@ def get_ws_results(url_list):
     return web_scraping_results
 
 def get_product_id_and_urls():
-    conn = sqlite3.connect('price_tracking_app.db')
+    conn = sqlite3.connect('price_tracker.db')
     cur = conn.cursor()
     product_id_and_urls = cur.execute(
-        '''
-        SELECT product_id,url FROM product_details p
-        WHERE p.email_notification = 'True'
+    '''
+        SELECT product_id,url FROM products p
+        WHERE p.email_pref = 'True'
     ''')
+    cursor.close()
+    conn.close()
     return product_id_and_urls #check if this returns a list of lists
 
 def prepare_results_for_db(ws_results, product_id_and_urls):
@@ -46,9 +43,11 @@ def prepare_results_for_db(ws_results, product_id_and_urls):
     return tuple_results
 
 def insert_results_db(tuple_results):
-    conn = sqlite3.connect('price_tracking_app.db') 
+    conn = sqlite3.connect('price_tracker.db') 
     cur = conn.cursor()
     cur.executemany('''
-                INSERT INTO webscraping_results(product_id,price,,currency, timestamp)
+                INSERT INTO price_history(product_id,price,currency,timestamp)
                 VALUES (?, ?, ?, ?)''', tuple_results)
     conn.commit()
+    cursor.close()
+    conn.close()
