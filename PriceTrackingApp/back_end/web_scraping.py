8840dbdfcff42178f4f923f7483f492790ab537f
vivilivi <violeta.pereda@gmail.com>
2024-08-20 00:17:52 +0200

    introduce small changes after unittesting

--- PriceTrackingApp/back_end/web_scraping.py
+++ PriceTrackingApp/back_end/web_scraping.py
@@ -10,8 +10,8 @@ class HTTPRequest:
     def __init__(self, url, logger):
         self.url = url
         self.headers = self.generate_header() # Identification string sent with the network request
-        self.soup = self.get_soup()
         self.logger = logger
+        self.soup = self.get_soup()
 
     def random_user_agent(self):
         a = os.path.dirname(__file__)
@@ -30,28 +30,29 @@ class HTTPRequest:
             'Connection': 'close', # If network connection stays open after the current transaction finishes
             'Referer': 'https://google.com'
         }
-
+    
     def get_soup(self):
         response = requests.get(self.url, headers=self.headers)
 
         if response.status_code != 200:
-            self.logger.write_log(f"""Error connecting to the url - ERROR: {response.status_code} - URL: {url}')
+            self.logger.write_log(f"""
+Error connecting to the url - ERROR: {response.status_code} - URL: {self.url}')
 ----------------------------------------------------------------
 """)
-            raise Exception
+            return None
 
         return BeautifulSoup(response.content, 'html.parser')
 
-class Logger: #to keep logs of errors, disabled to users per default
+class Logger: 
 
-    def __init__(self,timestamp, enabled=False):
+    def __init__(self,timestamp, enabled = True):
         self.enabled = enabled
         self.log_filename = timestamp + '.txt' #records info retrieved from page content
         self.debug_log_filename = timestamp + '_debug.txt' #records page content 
 
     def __write(self, filename, msg):
         if self.enabled:
-            with open('logs/' + filename, 'a', encoding="utf-8") as log_file:
+            with open('back_end/logs/' + filename, 'a', encoding="utf-8") as log_file:
                 log_file.write(msg)
 
     def write_log(self, msg):
@@ -63,7 +64,7 @@ class Logger: #to keep logs of errors, disabled to users per default
 class WebScraping:
 
     def __init__(self, url_list):
-        self.logger = Logger(timestamp=datetime.now().strftime("%Y-%m-%d %H-%M")) #add 'enabled=True' as parameter to debug and create error log
+        self.logger = Logger(timestamp=datetime.now().strftime("%Y-%m-%d %H-%M"))
         self.url_list = url_list
         self.web_scraping_results = []
 
@@ -71,15 +72,17 @@ class WebScraping:
         index = 1
         for url in self.url_list:
 
-            if 'amazon' in url.lower() or 'evapchiri' in url.lower(): #added evapchiri to include our mock wesite
+            if 'amazon' in url.lower() or 'evapchiri' in url.lower():   #added evapchiri to include our mock wesite
                 scraper = AmazonWebScraper(url, self.logger)
                 title, price, currency = scraper.get_all()
                 if title is None or price is None or currency is None:
-                    self.logger.write_log(f"""WebScraping.get_products_data - Information could not be retrieved.
+                    self.logger.write_log(f"""
+WebScraping.get_products_data - Information could not be retrieved.
 URL: {url}, TITLE: {title}, PRICE: {price}, CURRENCY: {currency}
 ----------------------------------------------------------------
 """)
-                    self.logger.write_debug_log(f"""URL: {url}
+                    self.logger.write_debug_log(f"""
+URL: {url}
 PAGE CONTENT: 
 {scraper.soup}
 ----------------------------------------------------------------
@@ -87,7 +90,8 @@ PAGE CONTENT:
                     continue
         
             else:
-                self.logger.write_log(f"""Website not supported - URL: {url}
+                self.logger.write_log(f"""
+Website not supported - URL: {url}
 ----------------------------------------------------------------
 """)
                 continue
@@ -116,6 +120,8 @@ class AmazonWebScraper(WebScraping):
 
     def get_title(self):
         try:
+            if self.soup is None:
+                return None
             title = self.soup.find(id='productTitle').get_text().strip()
         except AttributeError:
             return None
@@ -123,8 +129,10 @@ class AmazonWebScraper(WebScraping):
 
     def get_price(self):
         try:
-            price_whole = self.soup.find('span', class_ = 'a-price-whole').get_text(strip=True)
-            price_decimal = self.soup.find('span', class_ = 'a-price-fraction').get_text(strip=True)
+            if self.soup is None:
+                return None, None
+            price_whole = self.soup.find('span', class_ = 'a-price-whole').get_text(strip = True)
+            price_decimal = self.soup.find('span', class_ = 'a-price-fraction').get_text(strip = True)
             currency = self.soup.find('span', class_ = 'a-price-symbol').text
 
         except AttributeError:
@@ -138,6 +146,6 @@ class AmazonWebScraper(WebScraping):
 
 
 # if __name__ == "__main__":
-#     ws = WebScraping(['https://evapchiri.github.io/test_websiteCFG/'])
+#     ws = WebScraping(['https://evapchiri.github.io/test_websiteCFG/', 'invalid'])
 #     ws_results_FE = ws.get_product_data()
 #     print(ws_results_FE)
