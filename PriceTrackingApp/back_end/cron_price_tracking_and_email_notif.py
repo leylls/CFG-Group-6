2c0771022a9239747cb63b9bd1c349ac55c66e07
Leyla Bush <leylabush@gmail.com>
2024-08-21 11:10:17 +0100

    Add email notification functions to cron_price_tracking_and_email_notif.py

--- PriceTrackingApp/back_end/cron_price_tracking_and_email_notif.py
+++ PriceTrackingApp/back_end/cron_price_tracking_and_email_notif.py
@@ -1,8 +1,7 @@
 from back_end.web_scraping import WebScraping
 from back_end.web_scraping_db_interactions import WebscrapingDbInteractions
-# from email_api import PriceAlert
-# from email_api_db_interaction import notify_user_from_db
-
+from email_api import PriceAlert
+from email_api_db_interaction import notify_user_from_db
 
 def get_ws_results(url_list):
     ws = WebScraping(url_list)
@@ -22,15 +21,9 @@ def prepare_results_for_db(ws_results, product_id_and_urls):
 def check_price_below_threshold(current_price, threshold_price):
     return float(current_price) < float(threshold_price)
 
-def send_price_alert_email():
+def send_price_alert_email(price_alert, check_price_below_threshold):
     print("Activating price alert email notification...")
-    # initialise PriceAlert with API credentials
-    price_alert = PriceAlert(
-        api_key= 'your_api_key',
-        api_secret='your_api_secret',
-        sender_email='group6.cfgdegree24@gmail.com'
-    )
-    notify_user_from_db(price_alert)
+    notify_user_from_db(price_alert, check_price_below_threshold)
     print("Price alert email process completed.")
 
 
@@ -40,4 +33,11 @@ def cron_job_run():
     product_id_and_url = WebscrapingDbInteractions.get_product_id_and_urls()    # Obtain corresponding product_id for each url
     tuple_results = prepare_results_for_db (web_scraping, product_id_and_url)   # Map each url to it product_id & include pricing information retrieved
     WebscrapingDbInteractions.insert_ws_results_db(tuple_results)                  # Store new web scraping results in DB
-    #add email notification functions
+    # email notification functions
+    price_alert = PriceAlert(                                                   # Initialise with API credentials
+        api_key='your_api_key',
+        api_secret='your_api_secret',
+        sender_email='group6.cfgdegree24@gmail.com'
+    )
+    send_price_alert_email(price_alert, check_price_below_threshold)            # Send email is latest price is below threshold price
+
